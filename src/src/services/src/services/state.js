7da8bb23748b196910728a2118efa163aefe84f9
leober74 <leober.info@gmail.com>
2026-03-12 00:55:55 +0300

    Create state.js

---
+++ src/src/services/src/services/state.js
@@ -0,0 +1,23 @@
+const states = {};
+
+function getState(userId) {
+  return states[userId] || {};
+}
+
+function setState(userId, data) {
+  states[userId] = { ...states[userId], ...data };
+}
+
+function clearState(userId) {
+  delete states[userId];
+}
+
+function getStep(userId) {
+  return states[userId]?.step || null;
+}
+
+function setStep(userId, step) {
+  setState(userId, { step });
+}
+
+module.exports = { getState, setState, clearState, getStep, setStep };
